b25798e43288f57424db3f71f6e0017df387095c
Antoine2357 <antoine.jacqmin@live.fr>
2019-04-08 10:20:12 +0200

    quetes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { Route, BrowserRouter, Switch, NavLink } from 'react-router-dom';
+import Home from "./cont/Home"
+import History from "./cont/History"
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <BrowserRouter>
+
+      <NavLink exact to="/"> Accueil </NavLink>
+      <NavLink to="/notre-histoire" activeClassName="selected" > History </NavLink>
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/notre-histoire" component={History} />
+        </Switch>
+      </BrowserRouter>
+  );
   }
 }
 
---
+++ src/cont/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+              <p>Bienvenue</p>
+            </div>
+        );
+    }
+}
+
+export default Home;
